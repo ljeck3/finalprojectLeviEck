c22567b3fbf93ccddadcd2998e70c9c5b395f234
levieck <ljeck3@mail.fhsu.edu>
2025-04-05 15:00:05 -0500

    detail added to urls and views

--- rsvp/views.py
+++ rsvp/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the rsvp index.")
+
+def detail(request, guest_id):
+    return HttpResponse("You're looking at guest %s." % guest_id)
