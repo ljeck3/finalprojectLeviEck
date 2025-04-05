4575ad015c5383a28a8964caa60395829b25aef1
levieck <ljeck3@mail.fhsu.edu>
2025-04-05 17:55:10 -0500

    guest model updated

--- rsvp/models.py
+++ rsvp/models.py
@@ -20,12 +20,13 @@ class Event(models.Model):
 
 
 class Guest(models.Model):
+    event = models.ForeignKey('Event', on_delete=models.CASCADE, related_name='guests', null=True)
     name = models.CharField(max_length=100)
     email = models.EmailField(blank=True)
-    is_invited = models.BooleanField(default=True)
+    #is_invited = models.BooleanField(default=True)
     has_rsvped = models.BooleanField(default=False)
-    attending = models.BooleanField(null=True, blank=True)  # Yes/No/Undecided
-    message = models.TextField(blank=True)
+    #attending = models.BooleanField(null=True, blank=True)  # Yes/No/Undecided
+    #message = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
