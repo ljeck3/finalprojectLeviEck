feef5dd0c7755a57e0b312455367a66ab16d63f6
levieck <ljeck3@mail.fhsu.edu>
2025-04-05 19:24:36 -0500

    Basic RSVP works now.

--- rsvp/views.py
+++ rsvp/views.py
@@ -3,6 +3,7 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
 from .models import Event
+from .models import Guest
 
 
 #def index(request):
@@ -15,4 +16,24 @@ def index(request):
 
 def detail(request, event_id):
     event = get_object_or_404(Event, pk=event_id)
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+
+        #Debug to see if name and email go through
+        print(name, email)
+
+        try:
+            guest = Guest.objects.get(event=event, name=name, email=email)
+
+            if guest.has_rsvped:
+                message = "You have already RSVP'd for this event."
+            else:
+                guest.has_rsvped = True
+                guest.save()
+                message = "Thank you for RSVPing!"
+        except Guest.DoesNotExist:
+            message = "Sorry, you're not on the guest list for this event."
+
     return render(request, "rsvp/detail.html", {"event": event})
