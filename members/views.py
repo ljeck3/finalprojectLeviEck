b20c90890fbe227f4edcb6843a6589f6ee3a71d9
levieck <ljeck3@mail.fhsu.edu>
2025-04-06 21:13:38 -0500

    Added register.

--- members/views.py
+++ members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 def login_user(request):
     if request.method == 'POST':
@@ -21,3 +22,23 @@ def login_user(request):
 def logout_user(request):
     logout(request)  # This logs the user out
     return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Registration successful! Welcome, %s!' % user.username)
+            return redirect('home')  # Redirect to home page after successful registration
+        else:
+            messages.error(request, 'Please correct the errors below.')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'authenticate/register_user.html', {
+        'form': form,
+    })
