8f29fc15377cc077786d7b122a4baf8cb8a12876
levieck <ljeck3@mail.fhsu.edu>
2025-04-26 22:58:34 -0500

    Started Guestbook app. Up to admin interaction based on Django tutorial – new files added.

---
+++ guestbook/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+from django.contrib import admin
+
+class Message(models.Model):
+    name = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField("date published")
+
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
