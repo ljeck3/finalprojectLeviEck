8edd94810fde6b3e444c89ab68c5570f9d309a19
levieck <ljeck3@mail.fhsu.edu>
2025-04-28 14:42:42 -0500

    Messages display on guestbook index page.

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'guestbook'
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:message_id>/", views.detail, name="detail"),
+
 ]
--- guestbook/views.py
+++ guestbook/views.py
@@ -1,6 +1,16 @@
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
+
+from .models import Message
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the guestbook index.")
+    latest_message_list = Message.objects.order_by("-pub_date")[:5]
+    context = {"latest_message_list": latest_message_list}
+    return render(request, "guestbook/index.html", context)
 
+def detail(request, message_id):
+    message = get_object_or_404(Message, pk=message_id)
+    return render(request, "guestbook/detail.html", {"message": message})
